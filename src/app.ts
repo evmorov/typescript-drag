0a0fd779bfc993a31bfc43fd257948fea56c14bd
Evgeny Morozov <evmorov@gmail.com>
2020-12-30 00:39:20 +0300

    Wip

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-const app = document.querySelector('#app');
+const main = () => {
+  const app = document.querySelector('#app');
 
-if ('content' in document.createElement('template')) {
+  if (!isTemplateSupported()) {
+    renderTemplateNotSupported(app);
+    return;
+  }
+
   const projectInputTemplate = document.querySelector('#project-input') as HTMLTemplateElement;
-
   app.appendChild(projectInputTemplate.content.cloneNode(true));
-} else {
+
+  const singleProjectTemplate = document.querySelector('#single-project') as HTMLTemplateElement;
+  app.appendChild(singleProjectTemplate.content.cloneNode(true));
+
+  const projectListTemplate = document.querySelector('#project-list') as HTMLTemplateElement;
+  app.appendChild(projectListTemplate.content.cloneNode(true));
+};
+
+const isTemplateSupported = () => {
+  return 'content' in document.createElement('template');
+};
+
+const renderTemplateNotSupported = (app) => {
   app
     .appendChild(document.createElement('h1'))
     .appendChild(document.createTextNode('Your browser is not supported'));
-}
+};
+
+main();
